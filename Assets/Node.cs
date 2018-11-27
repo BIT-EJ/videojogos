b36cf02299ef4d04119392832ad2907215a2668f
victor augusto pozzan <victor_pozzan@hotmail.com>
2018-11-27 17:03:37 -0200

    Building but it is not working yet

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Node : MonoBehaviour {
 
     public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
 
     private Renderer rend;
     private Color startColor;
@@ -13,6 +16,21 @@ public class Node : MonoBehaviour {
     {
         rend = GetComponent<Renderer>();
     }
+
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't buil there! - TODO: Display on screen");
+            return;
+        }
+
+        //Build a turret 
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
 
     void OnMouseEnter()
     {
